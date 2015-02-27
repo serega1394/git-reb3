e43e379340710c1528e61bb4a9adbcd20ee59b60
Sergey Rozhkov <progger007@gmail.com>
2015-02-27 15:38:02 +0300

    Commit 12

--- runtests.bat
+++ runtests.bat
@@ -1,3 +1,4 @@
 call phpunit qtype_correctwriting_sequence_analyzer_test question/type/correctwriting/tests/test_sequence_analyzer.php
 call phpunit qtype_correctwriting_sequence_analyzer_test question/type/correctwriting/tests/test_grade_response.php
 call phpunit qtype_correctwriting_sequence_analyzer_test question/type/test_grade_response.php //commit 8
+1111 //commit 12
